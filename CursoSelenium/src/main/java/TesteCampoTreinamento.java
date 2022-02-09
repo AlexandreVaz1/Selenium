2836519cd146de9dbd6de8d41e649364bc2831cd
Alexandre Vaz <Alexandre Vaz@DESKTOP-ME38JN6>
2022-02-08 21:24:58 -0300

    Atualizando Novos Testes

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -1,10 +1,15 @@
+import java.util.List;
+
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 // http://wcaquino.me/selenium/componentes.html
 
 public class TesteCampoTreinamento {
@@ -59,4 +64,96 @@ public class TesteCampoTreinamento {
 			
 //		driver.quit();
 	}
+	
+	@Test
+	public void deveInteragirComCombo() {
+		WebDriver driver = new  FirefoxDriver();
+//		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+//		combo.selectByIndex(2);
+//		combo.selectByValue("superior");
+		combo.selectByVisibleText("Mestrado");	
+		Assert.assertEquals("Mestrado", combo.getFirstSelectedOption().getText());
+		driver.quit();
+	}
+
+	@Test
+	public void deveVerificarValoresCombo() {
+		WebDriver driver = new  FirefoxDriver();
+//		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select combo = new Select(element);
+		List<WebElement> options = combo.getOptions();
+		Assert.assertEquals(8, options.size());
+		
+		boolean encontrou = false;
+		for(WebElement option: options) {
+			if(option.getText().equals("Mestrado"));{
+				encontrou = true;
+				break; 
+			}
+		}
+	Assert.assertTrue(encontrou);
+	}
+	
+	@Test
+	public void deveVerificarValoresComboMultiplo() {
+		WebDriver driver = new  FirefoxDriver();
+//		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("Corrida");
+		combo.selectByVisibleText("O que eh esporte?");
+		
+		List<WebElement> allSelectedOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(3, allSelectedOptions.size());
+		
+		combo.deselectByVisibleText("Corrida");
+		allSelectedOptions = combo.getAllSelectedOptions();
+		Assert.assertEquals(2, allSelectedOptions.size());					
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComBotoes() {
+		WebDriver driver = new  FirefoxDriver();
+//		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement botao = driver.findElement(By.id("buttonSimple"));
+		botao.click();
+					
+		Assert.assertEquals("Obrigado!", botao.getAttribute("value"));
+		driver.quit();
+	}
+	
+	@Test
+	@Ignore
+	public void deveInteragirComLinks() {
+		WebDriver driver = new  FirefoxDriver();
+//		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.linkText("Voltar")).click();
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+

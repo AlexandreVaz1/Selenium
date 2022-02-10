420821407c2705378c58d598a34f2c50a02becce
Alexandre Vaz <Alexandre Vaz@DESKTOP-ME38JN6>
2022-02-09 21:52:31 -0300

    TestAlert

--- CursoSelenium/src/main/java/TesteCampoTreinamento.java
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -142,9 +142,22 @@ public class TesteCampoTreinamento {
 //		WebDriver driver = new ChromeDriver();
 		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
 		driver.findElement(By.linkText("Voltar")).click();
-		
+		Assert.assertEquals("Voltou!", driver.findElement(By.id("resultado")).getText());
+//		driver.quit();
+	}
+
+	@Test
+	public void deveBuscarTextosNaPagina() {
+		WebDriver driver = new  FirefoxDriver();
+//		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+//TodoTextoVisivel		System.out.println(driver.findElement(By.tagName("body")).getText());
+//		Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("Campo de Treinamento"));
+		Assert.assertEquals("Campo de Treinamento", driver.findElement(By.tagName("h3")).getText());
+		Assert.assertEquals("Cuidado onde clica, muitas armadilhas...", driver.findElement(By.className("facilAchar")).getText());
+		driver.quit(); 
 	}
 }
 
